e3c9a8ad30e418af3f0c82e7c1345fb2916784b9
kulesy <kulewidak@gmail.com>
2021-02-10 22:03:47 +1300

    iterating vector

--- cryptoTK.cpp
+++ cryptoTK.cpp
@@ -2,25 +2,63 @@
 #include <string>
 #include <vector>
 
+std::vector<std::string> morse = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", 
+"....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", 
+"...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."};
+
+std::vector<char> alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 
+'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' '};
+
 std::string morseEncode(std::string message) {
         
-std::vector<std::string> morse = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."};
-std::vector<char> alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' '};
-std::string encodedMessage = "";
+    std::string encodedMessage = "";
 
-for (int i = 0 ; i < message.size() ; i++) {
-    for (int j = 0 ; j < morse.size() ; j++) {
-        if (message[i] == alphabet[j]) {
-            encodedMessage += morse[j];
-            encodedMessage += " ";
-            break;
-        }
-        else if (message[i] == alphabet[26]) {
-            encodedMessage += "  ";
-            break;
+    for (int i = 0 ; i < message.size() ; i++) {
+        for (int j = 0 ; j < morse.size() ; j++) {
+            if (message[i] == alphabet[j]) {
+                encodedMessage += morse[j];
+                encodedMessage += " ";
+                break;
+            }
+            else if (message[i] == alphabet[26]) {
+                encodedMessage += "  ";
+                break;
+            }
         }
     }
-}
-return encodedMessage;
+    return encodedMessage;
 
 }
+
+std::string morseDecode(std::string message) {
+    
+    std::string decodedMessage = "";
+    int spaces = 0;
+    std::vector<std::string> morseUnits = {};
+    std::string unit = "";
+    for (int i = 0 ; i < message.size() ; i++) {
+        if (message[i] == alphabet[26]) {
+            for (int j = i ; message[i] == alphabet[26] ; i++) {
+                spaces += 1;
+            }
+            if (spaces == 1) {
+                morseUnits.push_back(unit);
+                spaces = 0;
+            }
+
+            else if (spaces == 3) {
+                i += 3;
+                morseUnits.push_back("   ");
+                spaces = 0;
+            }
+        }
+        else if (message[i] != alphabet[26]) {
+            unit += message[i];
+        }
+    }
+    
+    
+
+    
+}
+
